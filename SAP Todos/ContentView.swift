4c163ef87d2da1d9db871f753cfe268be5947757
ReyRachel8L <rlim.rachel08@gmail.com>
2022-06-25 10:15:34 +0800

    Initial Commit

---
+++ SAP Todos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAP Todos
+//
+//  Created by LIM YEE SIN RACHEL stu on 25/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAP Todos/SAP_TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_TodosApp.swift
+//  SAP Todos
+//
+//  Created by LIM YEE SIN RACHEL stu on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
